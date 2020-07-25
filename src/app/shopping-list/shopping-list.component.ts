2dbaf043d3587ea1b250effb09e20fb10819668e
Adel <htwr86@gmail.com>
2020-07-25 22:27:32 +0300

    10% Creating Components

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: 'shopping-list.component.html',
+})
+export class ShoppingList implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
