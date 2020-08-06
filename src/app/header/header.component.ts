7c8dd7291c07f14e00335ab24a09dfc16563d1dc
Adel <htwr86@gmail.com>
2020-08-06 08:11:53 +0300

    19% Adding Navigation with event emitting and *ngIf

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Output() pathSelected = new EventEmitter<string>();
+
+  onSelect(path: string) {
+    this.pathSelected.emit(path);
+  }
+}
